b20f72817a106e7b139c178b8b1e5bceced6dd41
TK <tk@MacBook-Air-de-TK.local>
2021-10-12 15:25:43 -0300

    chore: simple clients

--- clients/python/example.py
+++ clients/python/example.py
@@ -1,6 +1,6 @@
 from clickhouse_driver import Client
 
-client = Client(host='<host>', port=9441, user='<user>', password='<pass>', secure=True, ca_certs='root-ca.pem')
+client = Client(host='<host>', port=<port>, user='<user>', password='<pass>', secure=True, ca_certs='root-ca.pem')
 
 rows = client.execute('SHOW DATABASES')
 
